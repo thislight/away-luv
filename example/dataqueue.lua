f44dff2fb088b28a7bc2e7f6b0c59d81a4d84aa2
thisLight <l1589002388@gmail.com>
2020-09-02 15:55:56 +0800

    port to latest away.debugger

--- example/dataqueue.lua
+++ example/dataqueue.lua
@@ -16,6 +16,7 @@
 -- along with away-luv.  If not, see <http://www.gnu.org/licenses/>.
 local away = require 'away'
 local LuvService = require 'away.luv.service'
+local Debugger = require 'away.debugger'
 local Dataqueue = require('away.luv.dataqueue').dataqueue
 local Scheduler = away.scheduler
 
@@ -23,26 +24,12 @@ Scheduler:install(LuvService)
 
 local global_queue = Dataqueue:create()
 
-local function topstring(t)
-    if type(t) == 'table' then
-        local buffer =  {}
-        for k,v in pairs(t) do
-            table.insert(buffer, string.format("%s=%s", topstring(k), topstring(v)))
-        end
-        return '{'..table.concat(buffer, ',')..'}'
-    elseif type(t) == 'string' then
-        return '"'..t..'"'
-    else
-        return tostring(t)
-    end
-end
+Scheduler:add_watcher('push_signal', function(_, signal) if not signal.is_auto_signal then print('push_signal', Debugger.topstring(Debugger:pretty_signal(signal))) end end)
 
-Scheduler:add_watcher('push_signal', function(_, signal) if not signal.is_auto_signal then print('push_signal', topstring(signal)) end end)
-
-Scheduler:add_watcher('run_thread', function(_, thread, signal) if not signal.is_auto_signal then print('run_thread', thread,'signal', topstring(signal)) end end)
+Scheduler:add_watcher('run_thread', function(_, thread, signal) if not signal.is_auto_signal then print('run_thread', Debugger:remap_thread(thread),'signal', Debugger.topstring(Debugger:pretty_signal(signal))) end end)
 
 Scheduler:run_task(function()
-    while not global_queue:is_marked_end() do
+    while true do
         print('call next()')
         local val, err = global_queue:next()
         if val then
